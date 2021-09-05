5d61dd80817b35018c0dc2d2fa125ab041cea0a7
Rishi <rishiraj11a@gmail.com>
2021-09-05 20:55:06 +0530

    Adding 3D SHAPES

--- Areas_Volumes.cpp
+++ Areas_Volumes.cpp
@@ -15,7 +15,7 @@ int main()
     //********************AREAS OF SHAPES**********************************
 
 
-    if (w==1)
+    if (w==1)                      //2D SHAPES
     {
         cout<<"1. 2D Shapes"<<endl<<"2. 3D Shapes"<<endl;
         cout<<"Choose Any Type:";
@@ -26,11 +26,10 @@ int main()
     {
         cout<<"Here is the list of all 2D Shapes:"<<endl;
         cout<<"1. Square"<<endl<<"2. Rectangle"<<endl<<"3. Triangle"<<endl<<"4. Circle"<<endl<<"5. Parallelogram"<<endl<<"6. Trapezium"<<endl;
-
-        float a;
         cout<<"SELECT NUMBER, Area of which shape you want: ";
         cin>>a;
 
+        float a;
         float s, l, b, r, h;
 
         if (a==1)  //SQUARE
@@ -85,35 +84,89 @@ int main()
             cout<<"The Area of Trapezium is: "<<0.5 * (a+b) * h<<endl;
         }
         
-
         else
         {
             cout<<"Invalid Number Inputed"<<endl;
         }
     }
 
-    else if (x==2)
+
+    else if (x==2)                       //3D SHAPES
     {
-        cout<<"Here is the list of all 2D Shapes:"<<endl;
-        cout<<"1. Cube"<<endl<<"2. Cuboid"<<endl<<"3. Sphere"<<endl;
+        cout<<"Here is the list of all 3D Shapes:"<<endl;
+        cout<<"1. Cube"<<endl<<"2. Cuboid"<<endl<<"3. Sphere"<<endl<<"4. Hemisphere"<<endl<<"5. Cylinder"<<endl<<"6. Cone"<<endl;
+        cout<<"Choose any Shape:";
+        cin>>a;
+
+        float s,h,l,b,r;
+
+
+        if (a==1)  //CUBE
+        {
+            cout<<"Enter the value of side: ";
+            cin>>s;
+            cout<<"The Area of Cube is: "<<(s*s)*6<<endl;
+        }
+
+        else if (a==2)  //CUBOID
+        {
+            cout<<"Enter the value of length: ";
+            cin>>l;
+            cout<<"Enter the value of breadth: ";
+            cin>>b;
+            cout<<"Enter the value of hight: ";
+            cin>>h;
+            cout<<"The Area of Cuboid is: "<<((h*b)+(b*l)+(h*l))*2<<endl;
+        }
+
+        else if (a==3)   //SPHERE
+        {
+            cout<<"Enter the value of radius: ";
+            cin>>r;
+            cout<<"The Area of Circle is: "<<3.14 *r*r*4<<endl;
+        }
+
+        else if (a==4)   //HEMISPHERE
+        {
+            cout<<"Enter the value of radius: ";
+            cin>>r;
+            cout<<"The Area of Circle is: "<<3.14 * r*r*3<<endl;
+        }
+
+        else if (a==5)   //CYLINDER
+        {
+            cout<<"Enter the value of radius: ";
+            cin>>r;
+            cout<<"Enter the value of height: ";
+            cin>>h;
+            cout<<"The Area of Cylinder is: "<<(r+h)*3.14 * r*2<<endl;
+        }
+
+        else if (a==6)   //Cone
+        {
+            cout<<"Enter the value of radius: ";
+            cin>>r;
+            cout<<"Enter the value of slant height: ";
+            cin>>h;
+            cout<<"The Volume of Cone is: "<<(r+h)*3.14 *r*h<<endl;
+        }        
     }
     
-
 
 // **********************************VOLUMES OF SHAPES*************************************
 
 
     else if (w==2)
     {
+        float a;
+        float s, l, b, r, h;
+
+
         cout<<"Here is the List of all shapes:"<<endl;
         cout<<"1. Cube"<<endl<<"2. Cuboid"<<endl<<"3. Sphere"<<endl<<"4. Cylinder"<<endl<<"5. Right Circular Cone"<<endl<<"6. Prism"<<endl;
         cout<<"7. Pyramid"<<endl;
-
-        float a;
         cout<<"SELECT NUMBER, Volume of which shape you want: ";
         cin>>a;
-
-        float s, l, b, r, h;
 
         if (a==1)   // CUBE
         {
@@ -139,7 +192,7 @@ int main()
             cin>>r;
             cout<<"Enter the value of height: ";
             cin>>h;
-            cout<<"The Area of Sphere is: "<<(4/3) * 3.14 * r*r*r<<endl;
+            cout<<"The Volume of Sphere is: "<<(4/3) * 3.14 * r*r*r<<endl;
         }
 
         else if (a==4)   //CYLINDER
@@ -148,7 +201,7 @@ int main()
             cin>>r;
             cout<<"Enter the value of height: ";
             cin>>h;
-            cout<<"The Area of Cylinder is: "<<3.14 * r*r * h<<endl;
+            cout<<"The Volume of Cylinder is: "<<3.14 * r*r * h<<endl;
         }
 
         else if (a==5)   //Cone
@@ -157,7 +210,7 @@ int main()
             cin>>r;
             cout<<"Enter the value of height: ";
             cin>>h;
-            cout<<"The Area of Cone is: "<<(0.33) * 3.14 * r*r * h<<endl;
+            cout<<"The Volume of Cone is: "<<(0.33) * 3.14 * r*r * h<<endl;
         }
 
         else if (a==6)   //PRISM
@@ -166,7 +219,7 @@ int main()
             cin>>b;
             cout<<"Enter the value of height: ";
             cin>>h;
-            cout<<"The Area of Prism is: "<<b*h<<endl;
+            cout<<"The Volume of Prism is: "<<b*h<<endl;
         }
 
         else if (a==7)   //PYRAMID
@@ -175,10 +228,9 @@ int main()
             cin>>b;
             cout<<"Enter the value of height: ";
             cin>>h;
-            cout<<"The Area of Pyramid is: "<<(0.33) * b*h<<endl;
+            cout<<"The Volume of Pyramid is: "<<(0.33) * b*h<<endl;
         }
         
-
         else
         {
             cout<<"Invalid Number Inputed"<<endl;
